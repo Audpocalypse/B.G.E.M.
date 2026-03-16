739598434322a43b249e1f3be0b1a0a77a844d6c
Audpocalypse <cyfer22@gmail.com>
2026-03-16 14:55:28 -0700

    Update app metadata for v1.1.0

--- Dialogs/AboutDialog.cs
+++ Dialogs/AboutDialog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace Material_Editor.Dialogs
@@ -9,6 +10,7 @@ namespace Material_Editor.Dialogs
         public AboutDialog()
         {
             InitializeComponent();
+            aboutText.Text = BuildAboutText();
         }
 
         protected override void ApplyAppearance(AppearanceDefinition appearance)
@@ -27,6 +29,15 @@ namespace Material_Editor.Dialogs
             {
                 MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private static string BuildAboutText()
+        {
+            Assembly assembly = typeof(AboutDialog).Assembly;
+            string version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+                ?? assembly.GetName().Version?.ToString()
+                ?? Application.ProductVersion;
+            return $"B.G.E.M.{Environment.NewLine}Version {version}{Environment.NewLine}by Audpocalypse{Environment.NewLine}https://github.com/Audpocalypse/B.G.E.M./";
         }
     }
 }
